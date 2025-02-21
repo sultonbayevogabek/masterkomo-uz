343b047fb02e1476bb2e1e6623099362998d0625
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-02-21 22:56:08 +0500

    initial commit

---
+++ js/register.js
@@ -0,0 +1,34 @@
+'use strict';
+
+async function register() {
+  const statistics = new Statistics();
+  let user = JSON.parse(localStorage.getItem('user'));
+
+  document.querySelector('#joinToChannel')?.addEventListener('click', async () => {
+    await statistics.onClickTgBtn();
+    window.location.href = 'https://t.me/+d1JDpgATetViMGJi';
+  })
+
+  if (user && user?.name && user?.phone && user?.time) {
+    const formData = new FormData();
+
+    formData.append('Ismi', user?.name);
+    formData.append('Telefon raqami', user?.phone);
+    formData.append(`Ro'yxatdan o'tgan vaqti`, user?.time);
+    formData.append(`Foydalanuvchi ID`, statistics.userId);
+    formData.append(`Timestamp`, statistics.time?.toString());
+
+    let response = await fetch('https://script.google.com/macros/s/AKfycbwtTBIhtsSaxmKXTnPFMahevQntCzvlu-TIMehXUoegOEEu_raxbFzKJhifAKqbUxISPA/exec', {
+      method: 'POST',
+      body: formData
+    })
+    await response.json();
+    await statistics.onRegister(user);
+    localStorage.removeItem('user');
+  } else {
+    localStorage.removeItem('user');
+  }
+}
+
+register();
+
