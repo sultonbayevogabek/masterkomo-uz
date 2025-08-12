d1aac94ed4e00939a450e1458fe97a2375148a59
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-08-12 16:18:43 +0500

    commit

--- js/register.js
+++ js/register.js
@@ -1,16 +1,7 @@
 'use strict';
 
 (async _ => {
-  const statistics = new Statistics();
   let user = JSON.parse(localStorage.getItem('user'));
-
-  const joinButton = document.querySelector('#joinToChannel')
-  joinButton?.addEventListener('click', async (e) => {
-    joinButton.disabled = true;
-    await statistics.onClickTgBtn();
-    joinButton.disabled = false;
-    window.location.href = 'https://t.me/+9RjoYMCF4G41NjFi';
-  })
 
   if (user && user?.name && user?.phone && user?.time) {
     const formData = new FormData();
@@ -18,15 +9,12 @@
     formData.append('Ismi', user?.name);
     formData.append('Telefon raqami', user?.phone);
     formData.append(`Ro'yxatdan o'tgan vaqti`, user?.time);
-    formData.append(`Foydalanuvchi ID`, statistics.userId);
-    formData.append(`Timestamp`, statistics.time?.toString());
 
     let response = await fetch('https://script.google.com/macros/s/AKfycbzdFo3fILejY6EjjJyLjxPPcVv_WwEz-ikGsCa4GfqATLXldok84bSDsBFCEqngeYYw/exec', {
       method: 'POST',
       body: formData
     })
     await response.json();
-    await statistics.onRegister(user);
     localStorage.removeItem('user');
   } else {
     localStorage.removeItem('user');
